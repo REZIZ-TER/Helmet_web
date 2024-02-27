02831a67b588e664ff2bad8f59cbefd05892b6ba
REZIZ-TER <gamemode.film@gmail.com>
2024-02-28 02:20:06 +0700

    Creatcountdataindb

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const { MongoClient } = require('mongodb');
 //const port = 3000;
 let port = process.env.PORT || 3000;
 //const uri = 'mongodb://myAdmin:kasidate01@localhost:27017/';
-const uri = process.env.MONGODB_CONNECT_URI;
+const uri = "mongodb://myAdmin:kasidate01@localhost:27017/";
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
@@ -58,20 +58,60 @@ app.get('/', (req, res) => {
    res.sendFile(indexPath);
 });
 
-app.get('/getid/:date', async (req, res) => {
+// app.get('/getid/:date', async (req, res) => {
+//    const client = new MongoClient(uri);
+//    await client.connect();
+
+//    try {
+//      const selectedDate = req.params.date;
+//      const users = await client.db('SaveImages').collection('Images').find({ upload_time: selectedDate }).toArray();
+
+//      const base64Values = users.map((user) => user.image);
+//      res.status(200).send(base64Values);
+//    } finally {
+//      await client.close();
+//    }
+// });
+
+app.get('/getdate/:date', async (req, res) => {
    const client = new MongoClient(uri);
    await client.connect();
 
    try {
-     const selectedDate = req.params.date;
-     const users = await client.db('SaveImages').collection('Images').find({ upload_time: selectedDate }).toArray();
+       const selectedDate = req.params.date;
+       const month = new Date(selectedDate).getMonth() + 1; // Adding 1 because months are zero-based
 
-     const base64Values = users.map((user) => user.image);
-     res.status(200).send(base64Values);
+       const users = await client.db('SaveImages').collection('Images').find({ upload_time: selectedDate }).toArray();
+
+       const base64Values = users.map((user) => user.image);
+       res.status(200).send(base64Values);
+
+       // Save count to a collection for this month
+       const count = users.length;
+       const monthName = getMonthName(month);
+
+       const countCollection = client.db('SaveImages').collection(monthName);
+       const existingCount = await countCollection.findOne({ date: selectedDate });
+
+       if (existingCount) {
+           await countCollection.updateOne({ date: selectedDate }, { $set: { count } });
+       } else {
+           await countCollection.insertOne({ date: selectedDate, count });
+       }
    } finally {
-     await client.close();
+       await client.close();
    }
 });
+
+function getMonthName(month) {
+   const months = [
+       "January", "February", "March", "April", "May", "June",
+       "July", "August", "September", "October", "November", "December"
+   ];
+   return months[month - 1];
+}
+
+
 
 app.listen(port, () => {
    //console.log(`Example app listening at http://localhost:${port}`);
